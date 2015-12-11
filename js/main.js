317ad4b4bb704d7fcc920733710eb28ed213a182
Vincent Narbot <vnarbot@takeflight.io>
2015-12-11 17:27:24 -0500

    Can start playing, but no computer yet

--- js/main.js
+++ js/main.js
@@ -1,1 +1,63 @@
 // Noughts And Crosses
+
+var playerChoice 	= "";
+var computerChoice 	= "";
+
+jQuery(function($) {
+	//Init
+	$('#splash').show();
+	$('#game').hide();
+
+	//Selection X or O
+	$('.btn-x').click(function(e) {
+		if(playerChoice == "X"){
+			$(this).removeClass('active');
+			playerChoice = ""; 
+			computerChoice = "";
+			$('.btn-play').prop('disabled', true);
+		}
+		else {
+			$(this).addClass('active');
+			$('.btn-o').removeClass('active');
+			playerChoice = "X";
+			computerChoice = "O";
+			$('.btn-play').prop('disabled', false);
+		}
+	});
+
+	$('.btn-o').click(function(e) {
+		if(playerChoice == "O"){
+			$(this).removeClass('active');
+			playerChoice = ""; 
+			computerChoice = "";
+			$('.btn-play').prop('disabled', true);
+		}
+		else {
+			$(this).addClass('active');
+			$('.btn-x').removeClass('active');
+			playerChoice = "O";
+			computerChoice = "X";
+			$('.btn-play').prop('disabled', false);
+		}
+	});
+
+	//Start a game
+	$('.btn-play').click(function(e) {
+		$('#splash').hide();
+		$('#game').show();
+	});
+
+	$('.cell').click(function(e) {
+		if($(this).text() != ""){
+			$('.alert').text("Can't play here. Please try again !");
+		}
+		else {
+			$('.alert').text("");
+			$(this).text(playerChoice);
+		}
+	});
+
+
+
+
+});
